8e86fabbc85dc2dbe00a339e20fe584efc3076a8
Nathaniel Miller <dukeoflaser@gmail.com>
2016-01-03 23:37:22 +0000

    Done.

--- lib/project_euler_power_digit_sum.rb
+++ lib/project_euler_power_digit_sum.rb
@@ -1,1 +1,11 @@
 # code your solution here
+
+def power_digit_sum(x, n)
+  bucket = Array.new(n, x)
+  
+  final_product = bucket.inject { |product, power| product * power }
+  digits = final_product.to_s.chars.map(&:to_i)
+  total = digits.inject { |sum, digit|  sum + digit }
+  
+  total
+end
